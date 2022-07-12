d6401a11ede463c1fb1557041b1ed86ec851d8a3
Enrico-N <ngenricok@gmail.com>
2022-07-12 14:41:35 -0700

    fixed put request

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ update.addEventListener("click", (_) => {
     method: "put",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      name: "Darth Vadar",
+      name: "Darth Vader",
       quote: "I find your lack of faith disturbing.",
     }),
   });
